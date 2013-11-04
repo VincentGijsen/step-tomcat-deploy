35b19aa2ac6eb4db8c2ed50770838b543604b6d4
Vincent Gijsen <vtj.gijsen@gmail.com>
2013-11-04 16:52:15 +0100

    inital version

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ step-tomcat-deploy
 ==================
 
 This step deploy's a war file to tomcat, and restarts tomcat
+
+This is only tested on ubuntu-server 12.04 installation
+
+# Assumptions:
+* autoDeploy is enabled (default tomcat behaviour)
+* Both context dir and deploy dir exists and are writable by `deploy_user`
+
+
+# Options
+* deploy_host The host running tomcat/sshd
+* deploy_user (optional) The user (with sudo rights)
+* sshkey the ssh key used to authenticate as `deploy user`
+* war_file_source the path where to find the war-file (eg, after your build in target/)
+* war_file_destination where to deploy the war-file
+* context_descriptor_file the file used by tomcat to deploy the war under path `servlet_path`
+* service_name (optional) on ubuntu usually 'tomact7' (Default)
---
+++ run.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+set -e
+
+#VARIABLE Checking
+if [ -z "$WERCKER_TOMCAT_DEPLOY_HOST" ]; then
+    fail 'missing host option, please add it in your wercker.yml'
+fi
+
+USER="ubuntu"
+if [ -n "$WERCKER_TOMCAT_DEPLOY_USER" ]; then
+    USER=$WERCKER_TOMCAT_DEPLOY_USER	
+fi
+
+if [ -z "$WERCKER_TOMCAT_DEPLOY_SSHKEY" ]; then
+    fail 'missing sshkey option, please add it in your wercker.yml'
+fi
+
+if [ -z "$WERCKER_TOMCAT_DEPLOY_CONTEXT_DESCRIPTOR_FILE" ]; then
+    fail 'missing context_descriptor_file option, please add it in your wercker.yml'
+fi
+
+if [ -z "$WERCKER_TOMCAT_DEPLOY_WAR_FILE_DESTINATION" ]; then
+    fail 'missing war_file_destination option, please add it in your wercker.yml'
+fi
+
+if [ -z "$WERCKER_TOMCAT_DEPLOY_WAR_FILE_SOURCE" ]; then
+    fail 'missing war_file_source option, please add it in your wercker.yml'
+fi
+
+if [ -z "$WERCKER_TOMCAT_DEPLOY_SERVLET_PATH" ]; then
+    fail 'missing war_file_source option, please add it in your wercker.yml'
+fi
+
+SERVICE_NAME="tomcat7"
+if [ -n "$WERCKER_TOMCAT_DEPLOY_SERVICE_NAME" ]; then
+	SERVICE_NAME=$WERCKER_TOMCAT_DEPLOY_SERVICENAME
+fi
+
+
+#first deploy the (new) file
+deploy_path=`basename $WERCKER_TOMCAT_DEPLOY_CONTEXT_DESCRIPTOR_FILE`
+tmp_context_file=/tmp/`basename $WERCKER_TOMCAT_DEPLOY_CONTEXT_DESCRIPTOR_FILE`
+
+#create context file
+cat << EOF > $tmp_context_file	
+<Context docBase="$WERCKER_TOMCAT_DEPLOY_WAR_FILE_DESTINATION" path="$WERCKER_TOMCAT_DEPLOY_SERVLET_PATH" antiResourceLocking="false" reloadable="true" crossContext="true" allowLinking="true">
+	<Environment name="appEnvironment" value="$ENVIRONMENT" type="java.lang.String" override="false"/>
+</Context>
+EOF
+
+#copy the contextfile to the tomcat-server
+result=$(scp -i ${WERCKER_TOMCAT_DEPLOY_SSHKEY}  $tmp_context_file ${USER}@${WERCKER_TOMCAT_DEPLOY_HOST} )
+if [[ $? -ne 0 ]]; then
+    warning '$result'
+    fail 'context file copy failed';
+fi
+
+result=$(scp -i ${WERCKER_TOMCAT_DEPLOY_SSHKEY} $WERCKER_TOMCAT_DEPLOY_WAR_FILE_SOURCE ${USER}@${WERCKER_TOMCAT_DEPLOY_HOST}:${WERCKER_TOMCAT_DEPLOY_WAR_FILE_DESTINATION} )
+if [[ $? -ne 0 ]]; then
+    warning '$result'
+    fail 'Failed to copy warfile to server';
+fi
+
+result=$(ssh ${USER}@${WERCKER_TOMCAT_DEPLOY_HOST} -i ${WERCKER_TOMCAT_DEPLOY_SSHKEY} "sudo service $SERVICE_NAME restart")
+if [[ $? -ne 0 ]]; then
+    warning '$result'
+    fail 'Failed to restart tomcat';
+fi
+
