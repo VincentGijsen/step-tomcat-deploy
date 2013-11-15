66a718531cafcad9e888dd74c4b3049dfe0f9392
Vincent Gijsen <vtj.gijsen@gmail.com>
2013-11-15 10:48:41 +0100

    refactored temp names

--- run.sh
+++ run.sh
@@ -46,7 +46,8 @@ war_dst=$WERCKER_TOMCAT_DEPLOY_WAR_FILE_DESTINATION
 ctx_file=$WERCKER_TOMCAT_DEPLOY_CONTEXT_DESCRIPTOR_FILE
 srvlet_path=$WERCKER_TOMCAT_DEPLOY_SERVLET_PATH
 
-tmp_file_name="WERCKERFILE-"
+tmp_file_name_war="WERCKERFILE-war"
+tmp_file_name_ctx="WERCKERFILE-war"
 
 #first deploy the (new) file
 deploy_path=`basename $ctx_file`
@@ -68,7 +69,7 @@ info 'generated context file locally';
 #
 
 #copy the contextfile to the tomcat-server
-result=$(scp -i "$key" "$tmp_context_file" "$remote_user@$host:~/${tmp_file_name}-context" )
+result=$(scp -i "$key" "$tmp_context_file" "$remote_user@$host:~/${tmp_file_name_ctx}" )
 if [[ $? -ne 0 ]]; then
     warning '$result'
     fail 'context file copy failed';
@@ -77,7 +78,7 @@ else
 fi
 
 #copy the war file to the tomcat-server
-result=$(scp -i "$key" "$war_src"  "$remote_user@$host:~/${tmp_file_name}-war" )
+result=$(scp -i "$key" "$war_src"  "$remote_user@$host:~/${tmp_file_name_war}" )
 if [[ $? -ne 0 ]]; then
     warning '$result'
     fail 'Failed to copy warfile to server';
@@ -92,7 +93,7 @@ fi
 
 
 #Move the CONTEXT file on the remote file to the proper location using sudo
-result=$(ssh "$remote_user@$host" -i "$key" "sudo mv ~/${tmp_file_name}-context $ctx_file" )
+result=$(ssh "$remote_user@$host" -i "$key" "sudo mv ~/${tmp_file_name_ctx} $ctx_file" )
 if [[ $? -ne 0 ]]; then
     warning '$result'
     fail 'Failed to move context in right location';
@@ -102,7 +103,7 @@ fi
 
 
 #Move the war file on the remote file to the proper location using sudo
-result=$(ssh "$remote_user@$host" -i "$key" "sudo mv ~/${tmp_file_name}-war $war_dst" )
+result=$(ssh "$remote_user@$host" -i "$key" "sudo mv ~/${tmp_file_name_war} $war_dst" )
 if [[ $? -ne 0 ]]; then
     warning '$result'
     fail 'Failed more war file in right location';
